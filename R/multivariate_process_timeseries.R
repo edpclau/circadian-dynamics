bbdb7a5155364bb6d308c0647d4b8be5967d5afe
edpclau <65506362+edpclau@users.noreply.github.com>
2020-06-20 17:47:58 -0400

    Update multivariate_process_timeseries.R
    Integration of the butterworth into the pipeline.

--- R/multivariate_process_timeseries.R
+++ R/multivariate_process_timeseries.R
@@ -7,15 +7,15 @@
 #' (required) A character string indicating the sampling rate of the data. Examples: '30 minutes', '1 hour', '4 seconds', '100 days'.
 #' @param window_size_in_days (default=3) a numeric indicating the width of the window size in day units.
 #' @param window_step_in_days (default=1) a numeric indicating the amount of day by which to move the window in day units.
-#' @param smooth_data Logical. If TRUE (default) will smooth the measurement values useing a moving average. If FALSE measurement values won't be smoothed.
+#' @param movavg Logical. If TRUE will smooth the measurement values useing a moving average. If FALSE (default) measurement values won't be smoothed.
 #' @param detrend_data Logical. If TRUE (default) will detrend the data. If FALSE measurement values won't be detrended. If both, detrend_data and smooth_data are TRUE, the detrending will run over the smoothed data.
 #' @param butterworth Logical. If TRUE (default) will apply a butterworth filter to the measurement values using a moving average. If FALSE measurement values won't be filtered.
 #'
-#' @param binning_n A numeric which indicated the amount of bins over which to run the smoothing average. Default = 4.
+#' @param smoothing_n A numeric which indicated the amount of bins over which to run the smoothing average. Default = 4.
 #'
-#' @param period The period on which we want to draw the threshold (default = 24).
-#'
-#' @param type A string indicating if a low or high pass filter will be used. options: "low" (default), "high".
+#' @param order filter order. Default = 2.
+#' @param f_low Frequency for the low pass filter. Default = 1/4.
+#' @param f_high Frequency for the high pass filter. Default = 1/72.
 #'
 #' @param plot logical. If TRUE (default) plots the filtered data over the raw data. If FALSE, does not plot.
 #' @return
@@ -30,9 +30,12 @@
 #' window_step_in_days = 1, smooth_data = TRUE,
 #' detrend_data = TRUE)
 #'
+#' @importFrom lubridate is.POSIXct
+#' @importFrom purrr map
+#'
 multivariate_process_timeseries <- function(df = NULL, sampling_rate = NULL, window_size_in_days = 3,
-                                window_step_in_days = 1, smooth_data = TRUE, detrend_data = TRUE,
-                                butterworth = TRUE, period = 24, binning_n = 4, type = "low", plot = TRUE) {
+                                window_step_in_days = 1, movavg = FALSE, detrend_data = TRUE,
+                                butterworth = FALSE, f_low = 1/4, f_high = 1/73, order = 2, smoothing_n = 4, plot = TRUE) {
 ###### Flow control parameters######
 #1. Must supply a data frame.
 if (is.null(df)) {
@@ -48,8 +51,9 @@ if (!lubridate::is.POSIXct(df[[1]])) {
 
 processed_df <- purrr::map(2:ncol(df),
            .f = ~ process_timeseries(df = df[,c(1,.)], sampling_rate = sampling_rate, window_size_in_days = window_size_in_days,
-                                     window_step_in_days = window_step_in_days, smooth_data = smooth_data, detrend_data = detrend_data,
-                                     butterworth = butterworth, period = period, binning_n = binning_n, type = type, plot = plot))
+                                     window_step_in_days = window_step_in_days, movavg = movavg, detrend_data = detrend_data,
+                                     butterworth = butterworth, f_low = f_low, f_high = f_high, order = order,
+                                     smoothing_n = smoothing_n, plot = plot))
 
 names(processed_df) <- names(df)[-1]
 
