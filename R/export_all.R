4c294fed7cab2230093f8982085230badeda7c05
edpclau <65506362+edpclau@users.noreply.github.com>
2020-07-16 21:30:58 -0400

    Update export_all.R
    adds arguments to handle the new changes in the functions that are under the hood.

--- R/export_all.R
+++ R/export_all.R
@@ -8,6 +8,7 @@
 #' @param raw_data data.frame or tibble containing the raw data.
 #' @param processed_data data.frame returned from the process_timeseries or multivariate_process_timeseries functions.
 #' @param rythm_analysis_data data.frame returned from the rythm_analysis_by_window and multivariate_rythm_analysis functions.
+#'@param ld_column An integer which indicates the column of the raw_data which contains the light/dark data. (defult = NULL)
 #' @param autocorrelation If TRUE (default) plots autocorrelation. FALSE does not plot autocorrelation.
 #' @param lomb_scargle If TRUE (default) plots lomb scargle periodogram. FALSE does not plot periodogram.
 #' @param cosinor_fit Method to fit the COSINOR to the data. Either "lomb_scargle" (default) or "autocorrelation".
@@ -23,7 +24,7 @@
 #' export_all(processed_data = processed_data, rythm_analysis_data = analysis_data, new_dir_name = "test")
 #'
 #' @importFrom rstudioapi selectDirectory
-export_all <- function(raw_data = NULL, processed_data = NULL, rythm_analysis_data = NULL,
+export_all <- function(raw_data = NULL, processed_data = NULL, rythm_analysis_data = NULL, ld_column = NULL,
                        autocorrelation = TRUE, lomb_scargle = TRUE,
                        cosinor_fit = c("lomb_scargle", "autocorrelation"),
                        dir_choose_gui = TRUE, new_dir_name = "analysis") {
@@ -31,6 +32,9 @@ export_all <- function(raw_data = NULL, processed_data = NULL, rythm_analysis_da
 ##### Flow Control ####)
 if (is.null(processed_data)) { stop("Must provide the output from 'multivariate_process_timeseries'.")}
 if (is.null(rythm_analysis_data)) { stop("Must provide the output from 'multivariate_rythm_analysis'.")}
+if (is.null(raw_data)) { stop("Must provide the raw data.")}
+
+
 
 # You can choose between lomb_scargle or autocorrelation for "method". Period is the default.
 cosinor_fit <- base::match.arg(cosinor_fit, choices = c("lomb_scargle", "autocorrelation"))
@@ -47,10 +51,14 @@ if (dir_choose_gui) {
   dir.create(new_dir1)
 }
 
-
+#Create a 'raw data'column for the mean
+raw_data <- dplyr::left_join(raw_data, dplyr::distinct(processed_data$mean[2:3]), by = "datetime")
+raw_data <- dplyr::select(raw_data,1, ld_column, mean, dplyr::everything())
 if (!is.null(raw_data)) {
 setwd(new_dir1)
-plot_actogram(raw_data, export = TRUE)
+
+plot_actogram(raw_data, ld_column = ld_column, export = TRUE, autosize = TRUE)
+raw_data <- dplyr::select(raw_data, -ld_column)
 }
 
 bind_processed(processed_data, TRUE)
