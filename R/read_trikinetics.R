6fc5d59cbfc270470bb2ffc9b06b078d9881b63e
edpclau <65506362+edpclau@users.noreply.github.com>
2020-07-23 16:34:50 -0400

    New function read_trikinetics_folder

--- R/read_trikinetics.R
+++ R/read_trikinetics.R
@@ -1,6 +1,7 @@
 #' Import data from a Trikinetics tsv
 #' @param file a path to a Trikinetics file. If no path is supplied, a GUI will open and help with file selection.
-#' @export
+#' @export read_trikinetics
+#' @export read_trikinetics_folder
 #'
 #' @examples
 #' trikinetics_data <- read_trikinetics()
@@ -22,5 +23,25 @@ read_trikinetics <- function(file = NULL){
   df <- df[,-c(1,4:9)]
   df <- unite(df, "datetime", c(1:2), sep = " ") %>% mutate(datetime = parse_date_time(datetime, orders = "d m y H:M:S"))
   names(df) <- c("datetime", "dd", paste0("IND ",1:(ncol(df)-2)))
+  return(df)
+}
+
+read_tikinetics_folder <- function(directory = NULL) {
+
+message("Make sure, all monitors were run on the same dates with the same LD/DD settings.")
+
+  #### Flow Control ####
+  #Allow for using a GUI to choose the folder, if one is not supplied
+  if (is.null(directory)) {
+    directory <- rstudioapi::selectDirectory()
+  }
+
+  files <- list.files(directory)
+  paths <- paste0(directory, "/", files)
+  df <- suppressMessages(purrr::map(paths, read_trikinetics))
+  names(df) <- stringr::str_remove(files, "\\.txt")
+  df <- purrr::map_df(df, ~ tidyr::pivot_longer(., -c(1,2)), .id = "monitor")
+  df <-  tidyr::unite(df, "name", c(monitor, name), sep = " ")
+  df <- tidyr::pivot_wider(df, c(datetime, tidyr::matches("dd|ld")))
   return(df)
 }
