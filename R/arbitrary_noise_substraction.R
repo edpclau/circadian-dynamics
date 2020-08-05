d41ce2502fa1229dd63d3a635fbf654f60de9f1a
edpclau <65506362+edpclau@users.noreply.github.com>
2020-08-05 17:26:40 -0400

    New function to substract noise from data

---
+++ R/arbitrary_noise_substraction.R
@@ -0,0 +1,21 @@
+#' Subtract Arbitrarily Chosen Noise Level from the Data
+#' @usage arbitrary_noise_reduction(df, noise = 0)
+#' @param df a data.frame where the first column is a datetime object.
+#' @param noise an integer representing an arbitrarily chosen noise level to subtract from the data.
+#' Default = 0.
+#'
+#' @return
+#' @export
+#'
+#'
+arbitrary_noise_subtraction <- function(df, noise = 0) {
+
+noise_reduced <- map_df(df[-1], ~ ifelse(. == 0 | is.na(.), ., . - noise))
+
+noise_reduced$datetime <- df[[1]]
+
+noise_reduced <- dplyr::select(noise_reduced, datetime, dplyr::everything())
+
+return(noise_reduced)
+
+}
