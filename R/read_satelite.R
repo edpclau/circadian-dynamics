0896b4b6508e33e7024d4237daee8d18e40e4f9c
edpclau <65506362+edpclau@users.noreply.github.com>
2020-12-11 15:25:18 -0500

    Update read_satelite.R

--- R/read_satelite.R
+++ R/read_satelite.R
@@ -11,5 +11,6 @@ read_satellite <- function(path = NULL) {
   if (is.null(path)) path = rstudioapi::selectFile()
   df <- readr::read_csv(path, skip = 9)
   df$time <- lubridate::mdy_hm(df$time)
+  df <- dplyr::rename(df, 'datetime'=time)
   return(df)
 }
