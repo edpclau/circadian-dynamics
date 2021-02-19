74b47f2dcd6f665377c27f8a94d022f039612930
edpclau <65506362+edpclau@users.noreply.github.com>
2021-02-19 13:06:18 -0500

    Create print_version.R
    Print Circadian Dynamics version. Code taken from tidyverse

---
+++ R/print_version.R
@@ -0,0 +1,43 @@
+package_version <- function(x) {
+  version <- as.character(unclass(utils::packageVersion(x))[[1]])
+
+  if (length(version) > 3) {
+    version[4:length(version)] <- crayon::red(as.character(version[4:length(version)]))
+  }
+  paste0(version, collapse = ".")
+}
+
+msg <- function(..., startup = FALSE) {
+  if (startup) {
+    if (!isTRUE(getOption("tidyverse.quiet"))) {
+      packageStartupMessage(text_col(...))
+    }
+  } else {
+    message(text_col(...))
+  }
+}
+
+text_col <- function(x) {
+  # If RStudio not available, messages already printed in black
+  if (!rstudioapi::isAvailable()) {
+    return(x)
+  }
+
+  if (!rstudioapi::hasFun("getThemeInfo")) {
+    return(x)
+  }
+
+  theme <- rstudioapi::getThemeInfo()
+
+  if (isTRUE(theme$dark)) crayon::white(x) else crayon::black(x)
+
+}
+
+
+msg(
+  cli::rule(
+    left = crayon::bold("Attached:"),
+    right = paste0("Circadian Dynamics ", package_version("circadiandynamics"))
+  ),
+  startup = TRUE
+)
