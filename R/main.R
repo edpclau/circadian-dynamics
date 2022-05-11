c27af88d4d29870e788c385fd5f36b883e7ba658
Eddie Pérez <eddieperezclaudio@gmail.com>
2022-05-11 10:50:17 -0400

    Update main.R

--- R/main.R
+++ R/main.R
@@ -35,7 +35,11 @@
 #'
 #' @return A named list of data.frames containing the output of [circadiandynamics::butterworth], [find_gaps()], [makes_time_windows()], and [smooth_detrend_by_windows()] for each measurement value.
 #'
-#' @export
+#' @export process_timeseries.rmv_gaps
+#' @export process_timeseries.na_to_zero
+#' @export process_timeseries.waveform
+#' @export process_timeseries.main
+#' @export process_timeseries.core
 #'
 #' @examples
 #' processed_data <- process_timeseries(df = raw_data, sampling_rate = "30 min")
@@ -107,12 +111,11 @@ return(df)
 
 
 
-process_timeseries.main <- function(df = NULL,
+process_timeseries.core <- function(df = NULL,
 
                                     make_windows = FALSE,
                                     window_size_in_days = 3,
                                     window_step_in_days = 1,
-
                                     sampling_rate = '1 hour',
                                     detrend_data = TRUE,
                                     smooth_data = FALSE,
@@ -127,73 +130,127 @@ if (big_data) {
 plan(multisession)
 } else {plan(sequential)}
 
-if (make_windows){
-#Set step and window_size
-window_size <- days(window_size_in_days) #Width of the window
-times <- df$datetime
-step = seq(from = min(times), to = max(times), by = paste(window_step_in_days, "day")) #days to move the window
+  if (make_windows){
+  #Set step and window_size
+  window_size <- days(window_size_in_days) #Width of the window
+  times <- df$datetime
+  step = seq(from = min(times), to = max(times), by = paste(window_step_in_days, "day")) #days to move the window
 
-df = future_map(
-  .options = furrr_options(seed = TRUE, lazy = TRUE),
-  .x = step,
-  .f = ~ {
-    x = filter(df, (datetime >= .x) & (datetime <= .x + window_size))
-    #General Pipeline
-    x = process_timeseries.rmv_gaps(x, sampling_rate = sampling_rate)
-    x = process_timeseries.na_to_zero(x)
-    #ACF Pipeline
-    x = process_timeseries.waveform(x,
-                                    detrend_data = detrend_data, smooth_data = smooth_data,
-                                    butterworth = butterworth, f_low = f_low, f_high = f_high, order = order)
-    acf_results = analyze_timeseries.acf(x, from = from, to = to, sampling_rate = sampling_rate)
-    acf_cosinor = analyze_timeseries.cosinor(x, sampling_rate = sampling_rate, period = acf_results$period)
-    acf_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = acf_cosinor$wave, order = causal_order, period = acf_results$period)
-    #Lomb-Scargle Pipeline
-    lsp_results = analyze_timeseries.lomb(df = x, sampling_rate = sampling_rate, from = from, to = to)
-    lsp_cosinor = analyze_timeseries.cosinor(x, sampling_rate = sampling_rate, period = lsp_results$period)
-    lsp_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = lsp_cosinor$wave,  order = causal_order, period = acf_results$period)
+  df = future_map(
+    .options = furrr_options(seed = TRUE, lazy = TRUE),
+    .x = step,
+    .f = ~ {
 
-    return(list(data = x,
-                acf = list(results = acf_results,
-                           cosinor = acf_cosinor),
-                lomb = list(results = lsp_results,
-                            cosinor = lsp_cosinor)
-                )
-           )
-    }
-)
+      x = filter(df, (datetime >= .x) & (datetime <= .x + window_size))
 
-return(df)
+      #General Pipeline
 
-} else {
-#General Pipeline
-df = process_timeseries.rmv_gaps(df, sampling_rate = sampling_rate)
-df = process_timeseries.na_to_zero(df)
-#ACF Pipeline
-df = process_timeseries.waveform(df,
-                                detrend_data = detrend_data, smooth_data = smooth_data,
-                                butterworth = butterworth, f_low = f_low, f_high = f_high, order = order)
-acf_results = analyze_timeseries.acf(df, from = from, to = to, sampling_rate = sampling_rate)
-acf_cosinor = analyze_timeseries.cosinor(df, sampling_rate = sampling_rate, period = acf_results$period)
-acf_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = acf_cosinor$wave,  order = causal_order, period = acf_results$period)
-#Lomb-Scargle Pipeline
-lsp_results = analyze_timeseries.lomb(df = df, sampling_rate = sampling_rate, from = from, to = to)
-lsp_cosinor = analyze_timeseries.cosinor(df, sampling_rate = sampling_rate, period = lsp_results$period)
-lsp_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = lsp_cosinor$wave,  order = causal_order, period = acf_results$period)
+      x = process_timeseries.rmv_gaps(x, sampling_rate = sampling_rate)
+
+      x = process_timeseries.na_to_zero(x)
+
+      #ACF Pipeline
+      x = process_timeseries.waveform(x,
+                                      detrend_data = detrend_data, smooth_data = smooth_data,
+                                      butterworth = butterworth, f_low = f_low, f_high = f_high, order = order)
+
+      acf_results = analyze_timeseries.acf(x, from = from, to = to, sampling_rate = sampling_rate)
+
+      acf_cosinor = analyze_timeseries.cosinor(x, sampling_rate = sampling_rate, period = acf_results$period)
+
+      acf_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = acf_cosinor$wave, order = causal_order, period = acf_results$period)
+      #Lomb-Scargle Pipeline
+
+      lsp_results = analyze_timeseries.lomb(df = x, sampling_rate = sampling_rate, from = from, to = to)
+
+      lsp_cosinor = analyze_timeseries.cosinor(x, sampling_rate = sampling_rate, period = lsp_results$period)
+
+      lsp_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = lsp_cosinor$wave,  order = causal_order, period = acf_results$period)
+      return(list(data = x,
+                  acf = list(results = acf_results,
+                             cosinor = acf_cosinor),
+                  lomb = list(results = lsp_results,
+                              cosinor = lsp_cosinor)
+                  )
+             )
+      }
+  )
+
+  return(df)
+
+  } else {
+  #General Pipeline
+  df = process_timeseries.rmv_gaps(df, sampling_rate = sampling_rate)
+  df = process_timeseries.na_to_zero(df)
+  #ACF Pipeline
+  df = process_timeseries.waveform(df,
+                                  detrend_data = detrend_data, smooth_data = smooth_data,
+                                  butterworth = butterworth, f_low = f_low, f_high = f_high, order = order)
+  acf_results = analyze_timeseries.acf(df, from = from, to = to, sampling_rate = sampling_rate)
+  acf_cosinor = analyze_timeseries.cosinor(df, sampling_rate = sampling_rate, period = acf_results$period)
+  acf_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = acf_cosinor$wave,  order = causal_order, period = acf_results$period)
+  #Lomb-Scargle Pipeline
+  lsp_results = analyze_timeseries.lomb(df = df, sampling_rate = sampling_rate, from = from, to = to)
+  lsp_cosinor = analyze_timeseries.cosinor(df, sampling_rate = sampling_rate, period = lsp_results$period)
+  lsp_results$grangercausal = analyze_timeseries.grangertest(value = df$value, cos = lsp_cosinor$wave,  order = causal_order, period = acf_results$period)
 
 
-return(list(data = df,
-            acf = list(results = acf_results,
-                       cosinor = acf_cosinor),
-            lomb = list(results = lsp_results,
-                        cosinor = lsp_cosinor)
-            )
-       )
+  return(list(data = df,
+              acf = list(results = acf_results,
+                         cosinor = acf_cosinor),
+              lomb = list(results = lsp_results,
+                          cosinor = lsp_cosinor)
+              )
+         )
+
+  }
+
 
 }
 
+process_timeseries.main <- function(df = NULL,
+
+                                    make_windows = FALSE,
+                                    window_size_in_days = 3,
+                                    window_step_in_days = 1,
+                                    sampling_rate = '1 hour',
+                                    detrend_data = TRUE,
+                                    smooth_data = FALSE,
+                                    butterworth = TRUE,
+                                    f_low = 1/4,
+                                    f_high = 1/73,
+                                    order = 2,
+                                    causal_order = 1,
+                                    big_data = FALSE) {
+  if (big_data) {
+    plan(multisession)
+  } else {plan(sequential)}
+
+  return(
+
+    future_map(
+      .x = df,
+      .options = furrr_options(seed = TRUE, lazy = TRUE),
+      .f = ~ {
+        process_timeseries.core(df = .x,
+
+                                make_windows = make_windows,
+                                window_size_in_days = window_size_in_days,
+                                window_step_in_days = window_step_in_days,
+                                sampling_rate = sampling_rate,
+                                detrend_data = detrend_data,
+                                smooth_data = smooth_data,
+                                butterworth = butterworth,
+                                f_low = f_low,
+                                f_high = f_high,
+                                order = order,
+                                causal_order = causal_order,
+                                big_data = big_data)
+      }
+    )
+
+
+  )
 
 }
-
-
 
