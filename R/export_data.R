8ddc3dfde854ec4d4c5fde51b0ce9dfbea0e6240
Eddie Pérez <eddieperezclaudio@gmail.com>
2022-05-11 10:50:10 -0400

    Update export_data.R

--- R/export_data.R
+++ R/export_data.R
@@ -17,6 +17,7 @@
 #' @importFrom readr write_csv
 #' @importFrom dplyr select mutate
 #' @importFrom magrittr '%>%'
+#' @importFrom tidyr unnest
 export_data <- function(path = getwd(), processed_data = NULL, rythm_analysis_data = NULL) {
 
 
@@ -30,20 +31,36 @@ if (!is.null(processed_data))  {
 if (!is.null(rythm_analysis_data)) {
 df_select <-  dplyr::select(rythm_analysis_data,
               c(method, window, period, period_hours, power, lsp_p_value, sig_level, ofac, MESOR,
-                amplitude, amplitude_se, adj_r_squared, cosinor_p_value, phase_in_seconds)) %>%
+                amplitude, amplitude_se, adj_r_squared, cosinor_p_value, phase_in_seconds,
+                wave_x, wave_y)) %>%
   dplyr::rename(phase_in_hours = phase_in_seconds) %>%
   dplyr::rename(PR = adj_r_squared)
 
 df_autocor <- df_select %>% dplyr::filter(method == "autocorrelation") %>%
-  dplyr::select(-c(power, lsp_p_value, sig_level, ofac, method))
+  dplyr::select(-c(power, lsp_p_value, sig_level, ofac, method, wave_x, wave_y))
 
 df_lsp <- df_select %>% dplyr::filter(method == "lomb_scargle") %>%
-  dplyr::select(-c(period_hours, method))
+  dplyr::select(-c(period_hours, method, wave_x, wave_y))
+
 
 #Second time we write a csv
 write.csv(df_lsp, paste0(path, "/", "rythm_analysis_lsp.csv"), row.names = FALSE)
 #Third time we write a csv
 write.csv(df_autocor, paste0(path, "/", "rythm_analysis_autocor.csv"), row.names = FALSE)
+
+
+##### Export Cosinor Values ######
+df_autocor_wave <- df_select %>% dplyr::filter(method == "autocorrelation") %>%
+  dplyr::select(window, wave_x, wave_y) %>% unnest(cols = c(wave_x, wave_y))
+
+df_lsp_wave <- df_select %>% dplyr::filter(method == "lomb_scargle") %>%
+  dplyr::select(window, wave_x, wave_y) %>% unnest(cols = c(wave_x, wave_y))
+
+#Fourth time we write a csv
+write.csv(df_lsp_wave, paste0(path, "/", "lsp_cosinor.csv"), row.names = FALSE)
+#Fifth time we write a csv
+write.csv(df_autocor_wave, paste0(path, "/", "autocor_cosinor.csv"), row.names = FALSE)
+
 }
 
 }
