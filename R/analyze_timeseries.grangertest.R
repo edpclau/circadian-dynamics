66ce37ad4718b52250f6e144c798da14998721a5
Eddie Pérez <eddieperezclaudio@gmail.com>
2022-06-09 18:11:30 -0400

    Update analyze_timeseries.grangertest.R
    Create a tryCatch flow so that the analysis function continues to run even if the Granger test fails.

--- R/analyze_timeseries.grangertest.R
+++ R/analyze_timeseries.grangertest.R
@@ -20,12 +20,55 @@ analyze_timeseries.grangertest <- function(value, cos, order = 1, period) {
         )
       )
   } else {
+    rawdata_to_cos = tryCatch(
+        expr = grangertest(x = value, y = cos, order = order)[[4]][2],
+        error = function(e) {tryCatch(
+          expr = {
+            # message(paste('Failed Granger Test with causal order',order))
+            grangertest(x = value, y = cos, order = order+1)[[4]][2]
+          },
+          error = function(e) { tryCatch(
+            expr = {
+              # message(paste('Failed Granger Test with causal order',order+1))
+              grangertest(x = value, y = cos, order = order+2)[[4]][2]
+              },
+            error = function(e) {
+              # message(paste('Failed Granger Test with causal order',order+2))
+              # message('Possible Aliased coefficients. Try a larger order.')
+              message('Results will not Contain Granger Test due to Aliased Coefficients.')},
+            finally = return(NA)
+          )}
+        )}
+      )
+
+    cos_to_rawdata = tryCatch(
+      expr = grangertest(y = value, x = cos, order = order)[[4]][2],
+      error = function(e) {tryCatch(
+        expr = {
+          message(paste('Failed Granger Test with causal order',order))
+          grangertest(y = value, x = cos, order = order+1)[[4]][2]
+        },
+        error = function(e) { tryCatch(
+          expr = {
+            message(paste('Failed Granger Test with causal order',order+1))
+            grangertest(y = value, x = cos, order = order+2)[[4]][2]
+          },
+          error = function(e) {
+            message(paste('Failed Granger Test with causal order',order+2))
+            message('Possible Aliased coefficients. Try a larger order.')
+            message('Final Results will not Contain this test.')},
+          finally = return(NA)
+        )}
+      )}
+    )
+
+
 
 
   return(
     list(
-      rawdata_to_cos = grangertest(x = value, y = cos, order = order)[[4]][2],
-      cos_to_rawdata = grangertest(y = value, x = cos, order = order)[[4]][2]
+      rawdata_to_cos = rawdata_to_cos,
+      cos_to_rawdata = cos_to_rawdata
     )
   )
 
