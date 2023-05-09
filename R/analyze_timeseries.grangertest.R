cbce7c0329426944a9f240b1210e7f9cbb2b9c77
Eddie Pérez <eddieperezclaudio@gmail.com>
2023-05-09 18:06:35 -0400

    Update analyze_timeseries.grangertest.R

--- R/analyze_timeseries.grangertest.R
+++ R/analyze_timeseries.grangertest.R
@@ -9,7 +9,7 @@
 #'
 #' @importFrom lmtest grangertest
 #' @examples
-analyze_timeseries.grangertest <- function(value, cos, order = 1) {
+analyze_timeseries.grangertest <- function(value, cos, order = 2) {
 
   #if no cosinor, return nothing
   if (is_empty(cos)) {
@@ -23,46 +23,31 @@ analyze_timeseries.grangertest <- function(value, cos, order = 1) {
     value = ts(value)
     cos = ts(cos)
     rawdata_to_cos = tryCatch(
-        expr = grangertest(x = value, y = cos, order = order)[[4]][2],
-        error = function(e) {tryCatch(
-          expr = {
-            # message(paste('Failed Granger Test with causal order',order))
-            grangertest(x = value, y = cos, order = order+1)[[4]][2]
-          },
-          error = function(e) { tryCatch(
             expr = {
               # message(paste('Failed Granger Test with causal order',order+1))
-              grangertest(x = value, y = cos, order = order+2)[[4]][2]
+              grangertest(x = value, y = cos, order = order)[[4]][2]
               },
             error = function(e) {
               # message(paste('Failed Granger Test with causal order',order+2))
               # message('Possible Aliased coefficients. Try a larger order.')
-              message('Results will not Contain Granger Test due to Aliased Coefficients.')},
+              # message('Results will not Contain Granger Test due to Aliased Coefficients.')
+              return(NA)},
             finally = return(NA)
-          )}
-        )}
-      )
+          )
+
 
     cos_to_rawdata = tryCatch(
-      expr = grangertest(y = value, x = cos, order = order)[[4]][2],
-      error = function(e) {tryCatch(
-        expr = {
-          message(paste('Failed Granger Test with causal order',order))
-          grangertest(y = value, x = cos, order = order+1)[[4]][2]
-        },
-        error = function(e) { tryCatch(
           expr = {
-            message(paste('Failed Granger Test with causal order',order+1))
-            grangertest(y = value, x = cos, order = order+2)[[4]][2]
+            grangertest(y = value, x = cos, order = order)[[4]][2]
           },
           error = function(e) {
-            message(paste('Failed Granger Test with causal order',order+2))
-            message('Possible Aliased coefficients. Try a larger order.')
-            message('Final Results will not Contain this test.')},
+            # message('Final Results will not Contain this test.')
+            return(NA)},
           finally = return(NA)
-        )}
-      )}
     )
+    }
+
+
 
 
 
@@ -76,4 +61,3 @@ analyze_timeseries.grangertest <- function(value, cos, order = 1) {
 
   }
 
-}
